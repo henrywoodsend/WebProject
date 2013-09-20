21b2c63c0ac00b587047e12494360a319a0268b4
henrywoodsend <henrywoodsend@gmail.com>
2013-09-20 11:16:42 +0100

    Update HelloWorld.java
    Fixed hello world 
    Closed #1

--- WebProject/src/com/WebProject/HelloWorld.java
+++ WebProject/src/com/WebProject/HelloWorld.java
@@ -4,8 +4,6 @@ public class HelloWorld {
 	public HelloWorld(){
 		System.out.println("Hello World!");
 		
-		System.out.println("Add now, hello universe");
-		
 		System.out.println("Updated in GitHub!");
 		
 	}
